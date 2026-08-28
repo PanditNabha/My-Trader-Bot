bd4d68a0ab9391782c3097e5e13afbcf8b1888d2
PanditNabha <sharmapb00083@gmail.com>
2026-08-28 04:46:50 +0400

    Change leverage from 1 to 10

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ API_SECRET = 'EbKGn8vw2f1HHacJRENw8ydqoV60rSlGWKks4nTYGdD9A2SSpEUimhL3RvP2'
 SYMBOL = 'ETHUSD'
        # Delta Exchange trading pair
 TIMEFRAME = '1h'         # 15 Minute Scalping Timeframe
-LEVERAGE = 1              # Dynamic Lot Size multiplier / Leverage
+LEVERAGE = 10             # Dynamic Lot Size multiplier / Leverage
 
 # Strategy Parameters
 EMA_FAST = 9
